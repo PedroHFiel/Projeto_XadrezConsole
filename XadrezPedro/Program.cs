9915a25db6f2369e2d1e4078c040c310c0c4174b
PedroHFiel <pedrofiel444@gmail.com>
2023-08-01 18:48:18 -0300

    Implementando a logica de xequeMate

--- XadrezPedro/Program.cs
+++ XadrezPedro/Program.cs
@@ -2,16 +2,22 @@
 using tabuleiro;
 using xadrez;
 
-namespace xadrez_console {
-    class Program {
-        static void Main(string[] args) {
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-            try {
+            try
+            {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                while (!partida.terminada) {
+                while (!partida.terminada)
+                {
 
-                    try {
+                    try
+                    {
                         Console.Clear();
                         Tela.imprimirPartida(partida);
 
@@ -32,14 +38,16 @@ namespace xadrez_console {
 
                         partida.realizaJogada(origem, destino);
                     }
-                    catch (TabuleiroException e) {
+                    catch (TabuleiroException e)
+                    {
                         Console.WriteLine(e.Message);
                         Console.ReadLine();
                     }
                 }
 
             }
-            catch (TabuleiroException e) {
+            catch (TabuleiroException e)
+            {
                 Console.WriteLine(e.Message);
             }
 
